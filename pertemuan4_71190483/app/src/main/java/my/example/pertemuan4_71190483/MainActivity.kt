dbd0610b1fe7f818c2b9a3de9af6bdb66175e727
Andriyanus Prasetya Aji <andriyanus.prasetya@ti.ukdw.ac.id>
2022-03-07 19:25:37 +0700

    pertemuan4_71190483

---
+++ pertemuan4_71190483/app/src/main/java/my/example/pertemuan4_71190483/MainActivity.kt
@@ -0,0 +1,11 @@
+package my.example.pertemuan4_71190483
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
