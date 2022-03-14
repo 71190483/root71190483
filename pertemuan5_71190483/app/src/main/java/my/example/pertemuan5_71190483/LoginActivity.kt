0b3160af8761d20474872e01870d29ccf9b614c8
Andriyanus Prasetya Aji <andriyanus.prasetya@ti.ukdw.ac.id>
2022-03-14 18:14:26 +0700

    pertemuan5_71190483

---
+++ pertemuan5_71190483/app/src/main/java/my/example/pertemuan5_71190483/LoginActivity.kt
@@ -0,0 +1,36 @@
+package my.example.pertemuan5_71190483
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val txUsername : EditText = findViewById(R.id.txUsername)
+        val txPassword = findViewById<EditText>(R.id.txPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            login(txUsername.text.toString(), txPassword.text.toString())
+        }
+    }
+    fun login(username: String, password: String){
+        if (password.equals("1234")){
+            val i : Intent = Intent(this,MainActivity::class.java)
+            i.putExtra("user",username)
+            startActivity(i)
+        }else {
+            toast("Password Salah!, Silahkan Masukkan Kembali !")
+        }
+    }
+
+    fun toast(message: String) {
+        Toast.makeText(this,message, Toast.LENGTH_LONG).show()
+
+    }
+}
